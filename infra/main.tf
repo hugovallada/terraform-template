e0dbbf6fb018e419cc07806fea584df81cd17ac0
Hugo Vallada <valladahugo@gmail.com>
2022-09-21 07:37:22 -0300

    Configuração backend

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.31.0"
     }
   }
+
+  backend "s3" {
+    bucket = "hugolopes-terraform-state"
+    key    = "project/${nome_do_projeto}/terraform.tfstate" //TODO: Substituir pelo nome do projeto
+    region = "sa-east-1"
+  }
 }
 
 provider "aws" {
